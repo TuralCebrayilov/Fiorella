399e7ba84ac15feb7624a1658f22450328b26d70
TuralCebrayilov <cebrayiltural@gmail.com>
2023-04-14 11:21:42 +0400

    Fiorello

---
+++ Fiorella/Fiorella/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using Fiorella.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fiorella.Controllers
+{
+    public class HomeController : Controller
+    {
+      
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
